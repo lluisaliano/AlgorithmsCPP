7028d3f700391cb0abf8c1daef107eea0d313472
Lluís Aliaño Llufriu <lluisalillu@icloud.com>
2026-02-16 22:15:23 +0100

    Added SelectionSort swapping tracker optimization

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -8,13 +8,16 @@ int main() {
   constexpr int length{static_cast<int>(std::size(array))};
 
   for (int i{length}; i > 0; --i) {
+    bool swapped{false};
     int j{0};
     while (j + 1 < i) {
       if (array[j] > array[j + 1]) {
         std::swap(array[j], array[j + 1]);
+        swapped = true;
       }
       ++j;
     }
+    if (!swapped) break; // If there has not been any swap, array is already sorted
   }
 
   for (auto i{0}; i < length; ++i) {
